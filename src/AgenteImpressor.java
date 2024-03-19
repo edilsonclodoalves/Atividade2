b20a70a3523ffbc21a590839138d000252924461
Edilson <32214931@ulife.com.br>
2024-03-18 21:54:25 -0300

    Add new class

---
+++ src/AgenteImpressor.java
@@ -0,0 +1,9 @@
+import jade.core.Agent;
+
+public class AgenteImpressor extends Agent {
+    protected void setup() {
+        System.out.println("Olá! Eu sou um agente impressor!");
+        System.out.println("# Vou executar meu comportamento");
+        addBehaviour(new ImprimeFrase(this, 5000));
+    }
+}
--- src/MeuComportamento.java
+++ src/MeuComportamento.java
@@ -2,5 +2,19 @@ import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 
 public class MeuComportamento extends Behaviour {
-int i=0;
+    int i = 0;
+
+    public MeuComportamento(Agent a) {
+        super(a);
+    }
+
+    public void action() {
+        System.out.println("* Olá Mundo! ... Meu nome é " + myAgent.getLocalName());
+        i = i + 1;
+    }
+
+    public boolean done() {
+        //public este método retorne TRUE o comportamento será finalizado
+        return i > 3;
+    }
 }
